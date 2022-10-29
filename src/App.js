fc0c86918fafc8299a11f5189118fde160d33d59
iAdityarajpurohit <108542528+iAdityarajpurohit@users.noreply.github.com>
2022-10-29 14:45:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../src/App.css";
+
+ const App = () => {
+  return (
+    <div>
+      <p>Now i can render any React component on any DOM node i want</p>
+    </div>
+  )
+}
+
+export default App;
